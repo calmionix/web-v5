961f177cd57dda33c797f13f8a7ebe60e60f1128
calmionix <calmionix@gmail.com>
2026-03-11 21:14:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -282,7 +282,7 @@ function processScheduleData(scheduleData, jadwalContainer, slotContainer, total
 
     // Calculate statistics
     const totalAntrian = activeSchedules.length;
-    const maxSlotsPerWeek = 10;
+    const maxSlotsPerWeek = 30;
     const slotTersedia = Math.max(0, maxSlotsPerWeek - totalAntrian);
     const nextUpload = activeSchedules.length > 0 ? activeSchedules[0] : null;
 
